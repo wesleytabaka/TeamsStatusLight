d25a24b0e499dae8c9b270d7005e883a61e2bce6
Wesley Tabaka <wesley@wesleytabaka.com>
2024-11-30 14:49:17 -0500

    Define interfaces

---
+++ IIndicatorInstruction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamsStatusLight
+{
+    internal interface IIndicatorInstruction
+    {
+        int r {  get; set; }
+        int g { get; set; }
+        int b { get; set; }
+        Effect effect { get; set; }
+        int effectRate { get; set; }
+        Transition transition { get; set; }
+        int transitionDuration { get; set; }
+    }
+}
---
+++ IPresence.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamsStatusLight
+{
+    internal interface IPresence
+    {
+        string activity { get; set; }
+        string availability { get; set; }
+    }
+}
