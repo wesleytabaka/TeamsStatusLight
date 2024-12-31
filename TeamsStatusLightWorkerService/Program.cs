f88349e4adea32c534ec4f916a511389d430d6be
Wesley Tabaka <wesley@wesleytabaka.com>
2024-12-30 21:06:51 -0500

    Major refactoring. Should have created a project subfolder for TeamsStatusLight. Deleted both projects from Solution, moved TeamsStatusLight to a subfolder, then added back to Solution. Anyway, add a project for TeamsStatusLightWorkerService. Seems to compile. #11

--- TeamsStatusLightWorkerService/Program.cs
+++ TeamsStatusLightWorkerService/Program.cs
@@ -1,14 +1,22 @@
-namespace TeamsStatusLightWorkerService
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = Host.CreateApplicationBuilder(args);
-            builder.Services.AddHostedService<Worker>();
+using App.WindowsService;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
 
-            var host = builder.Build();
-            host.Run();
-        }
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddWindowsService(options => { 
+            options.ServiceName = "TeamsStatusLight Service";
+        });
+
+        LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+        builder.Services.AddSingleton<TeamsStatusLightService>();
+        builder.Services.AddHostedService<WindowsBackgroundService>();
+
+        var host = builder.Build();
+        host.Run();
     }
 }
--- TeamsStatusLightWorkerService/Worker.cs
+++ TeamsStatusLightWorkerService/Worker.cs
@@ -1,24 +1,23 @@
-namespace TeamsStatusLightWorkerService
+namespace App.WindowsService;
+
+public sealed class WindowsBackgroundService(TeamsStatusLightService teamsStatusLightService, ILogger<WindowsBackgroundService> _logger) : BackgroundService
 {
-    public class Worker : BackgroundService
+    //private readonly ILogger<Worker> _logger;
+
+    //public Worker(ILogger<Worker> logger)
+    //{
+    //    _logger = logger;
+    //}
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger)
+        while (!stoppingToken.IsCancellationRequested)
         {
-            _logger = logger;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
+            if (_logger.IsEnabled(LogLevel.Information))
             {
-                if (_logger.IsEnabled(LogLevel.Information))
-                {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                }
-                await Task.Delay(1000, stoppingToken);
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
+            await Task.Delay(1000, stoppingToken);
         }
     }
 }
