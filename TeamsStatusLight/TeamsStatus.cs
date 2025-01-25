a3fbabc102751ecb9db8c4852ace25b57af31c0f
Wesley Tabaka <wesley@wesleytabaka.com>
2025-01-25 18:06:48 -0500

    Use FileStream with FileShare mode ReadWrite so the Teams logs can be accessed while Teams is running. #16 Personal accounts have a 16 character user id.  Add this to Regex pattern. #17

--- TeamsStatusLight/TeamsStatus.cs
+++ TeamsStatusLight/TeamsStatus.cs
@@ -25,10 +25,23 @@ namespace TeamsStatusLight
                 // check that it matches the pattern 2024-11-27T20:18:06.744374-05:00 0x000047bc native_modules::UserDataCrossCloudModule: BroadcastGlobalState: New Global State Event: UserDataGlobalState total number of users: 1 {user id: , availability: , unread notification count: x}
 
                 // There's probably a more efficient way of reading the log backwards.  For now let's load the whole file line by line.
-                Regex regex = new Regex(@"^\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}[+|\-]\d{2}:\d{2} 0x[0-9a-f]{8} <INFO> native_modules::UserDataCrossCloudModule: BroadcastGlobalState: New Global State Event: UserDataGlobalState total number of users: \d{1,2} { user id :[0-9a-f]{8}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{12}, availability: (.+?), unread notification count: \d{1,2} }$");
+                Regex regex = new Regex(@"^\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}[+|\-]\d{2}:\d{2} 0x[0-9a-f]{8} <INFO> native_modules::UserDataCrossCloudModule: BroadcastGlobalState: New Global State Event: UserDataGlobalState total number of users: \d{1,2} { user id :(?:[0-9a-f]{8}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{12}|[0-9a-f]{16}), availability: (.+?), unread notification count: \d{1,2} }$");
 
                 try {
-                    string[] fs = File.ReadAllLines(logFile);
+                    FileStream fileStream = new FileStream(logFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                    int numBytesToRead = (int)fileStream.Length;
+                    byte[] bytes = new byte[numBytesToRead];
+                    int numBytesRead = 0;
+                    while(numBytesToRead > 0)
+                    {
+                        int n = fileStream.Read(bytes, numBytesRead, numBytesToRead);
+                        if (n == 0)
+                            break;
+                        numBytesRead += n;
+                        numBytesToRead -= n;
+                    }
+                    fileStream.Close();
+                    string[] fs = System.Text.Encoding.UTF8.GetString(bytes).Split("\r\n");
                     for (int l = fs.Length - 1; l >= 0; l--)
                     {
                         string s = fs[l];
