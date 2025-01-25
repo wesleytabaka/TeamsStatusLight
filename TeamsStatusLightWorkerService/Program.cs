aaa5ee95fadc76be2023f3fc985ac99020e42e84
Wesley Tabaka <wesley@wesleytabaka.com>
2025-01-25 16:55:35 -0500

    Do not automatically build installer.  Still need to figure out how to either auto publish or have installer project refer to release output. Working installer and uninstaller. Added install and uninstall flags to worker service Program.cs Still some items to resolve with installer.  User is prompted if they'd like to restart the service being uninstalled after installation.

--- TeamsStatusLightWorkerService/Program.cs
+++ TeamsStatusLightWorkerService/Program.cs
@@ -4,19 +4,58 @@ using Microsoft.Extensions.Logging.EventLog;
 
 public class Program
 {
+    const string ServiceName = "TeamsStatusLight Service";
     public static void Main(string[] args)
     {
-        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
-        builder.Services.AddWindowsService(options => { 
-            options.ServiceName = "TeamsStatusLight Service";
-        });
+        if (args.Length > 0) {
+            string exePath = Path.Combine(AppContext.BaseDirectory, "TeamsStatusLightWorkerService.exe");
 
-        LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
+            if (args[0] == "/Install") {
+                System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo("sc", $"create \"{ServiceName}\" binPath= \"{exePath}\"");
+                psi.RedirectStandardOutput = true;
+                psi.UseShellExecute = false;
+                psi.CreateNoWindow = true;
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = psi;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                Console.WriteLine(result);
+            }
+            if (args[0] == "/Uninstall") {
+                System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo("sc", $"stop \"{ServiceName}\"");
+                psi.RedirectStandardOutput = true;
+                psi.UseShellExecute = false;
+                psi.CreateNoWindow = true;
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = psi;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                Console.WriteLine(result);
+                
+                System.Diagnostics.ProcessStartInfo psi2 = new System.Diagnostics.ProcessStartInfo("sc", $"delete \"{ServiceName}\"");
+                psi2.RedirectStandardOutput = true;
+                psi2.UseShellExecute = false;
+                psi2.CreateNoWindow = true;
+                System.Diagnostics.Process proc2 = new System.Diagnostics.Process();
+                proc2.StartInfo = psi2;
+                proc2.Start();
+                string result2 = proc2.StandardOutput.ReadToEnd();
+                Console.WriteLine(result2);
+            }
+        }
+        else {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddWindowsService(options => {
+                options.ServiceName = ServiceName;
+            });
 
-        builder.Services.AddSingleton<TeamsStatusLightService>();
-        builder.Services.AddHostedService<WindowsBackgroundService>();
+            LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
 
-        var host = builder.Build();
-        host.Run();
+            builder.Services.AddSingleton<TeamsStatusLightService>();
+            builder.Services.AddHostedService<WindowsBackgroundService>();
+
+            var host = builder.Build();
+            host.Run();
+        }
     }
 }
